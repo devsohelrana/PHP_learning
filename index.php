310ae3e6985d9c726f295b7468ef2bbed2d21eee
devsohelrana <dev.sohelrana@gmail.com>
2024-02-13 21:16:05 +0600

    PHP syntex leraning done..

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Print something on display follow this syntex..
+echo 'Display....';
+echo "Display....";
+print 'Display...'; 
+
+// PHP veriable Declaring && define..
+// variable name cannot start with a number
+// veriable name only containe A-z, 0-9, and _ 
+
+$veriable_name = 'Value';
+$_variableName = 'Value';
+
+echo "Veriable store {$veriable_name}";
+
+
+
+
+?>
