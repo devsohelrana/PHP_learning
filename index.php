7d1d2e8226b3d75d31b2bf9c092aafbafebd6baa
devsohelrana <dev.sohelrana@gmail.com>
2024-02-15 20:17:25 +0600

    PHP arithmetic operators learning done inculuding full process

--- index.php
+++ index.php
@@ -17,24 +17,24 @@ $oilPrice = 190;
 $balance = 4000;
 $border = 5;
 
-$totalRiceOil = $rice + $oil; // useage addition (+) operators
+$totalRiceOil = $rice + $oil; // usage addition (+) operators
 
-$totalRicePrice = $rice * $ricePrice; // useage multiplication (*) operators
+$totalRicePrice = $rice * $ricePrice; // usage multiplication (*) operators
 $totalOilPrice = $oil * $oilPrice;
 
 $totalCost = $totalRicePrice + $totalOilPrice;
 
-$extraBalance = $balance - $totalCost; // useage subtraction (-) operators
+$extraBalance = $balance - $totalCost; // usage subtraction (-) operators
 
-$individualCost = $totalCost / $border; // useage division (/) operators
+$individualCost = $totalCost / $border; // usage division (/) operators
 
-$modulus = $rice % $oil; // useage modulus (%) operators
+$modulus = $rice % $oil; // usage modulus (%) operators
 
 # ** exponentiation operators work as power 2'th3
 $x = 9; // base value
 $y = 2; // power value 9 ** 2 = 81
 
-$exponentiation = $x ** $y; // useage exponentiation (**) operators
+$exponentiation = $x ** $y; // usage exponentiation (**) operators
 
 # display any variable value
 echo $exponentiation;
